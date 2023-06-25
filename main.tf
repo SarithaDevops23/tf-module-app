d003f91ecf31a85d3cce12a6130e8299de678d8a
sarithadevops23@gmail.com <sarithadevops23@gmail.com>
2023-06-25 16:35:01 +0530

    init

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
 resource "null_resource" "test"{
+	triggers={
+		xyz=timestamp()
+	}
 	provisioner "local-exec"{
-		command="Hello world - env - ${var.env}"
+		command = "Hello world - env - ${var.env}"
 	}
 }
